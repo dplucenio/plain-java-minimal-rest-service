e7623e876001e116be7f4aa5f99a4612636d47aa
Daniel Plucenio <dplucenio@gmail.com>
2020-08-06 23:34:22 -0300

    First commit

---
+++ src/main/java/io/plucen/Main.java
@@ -0,0 +1,36 @@
+package io.plucen;
+
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.Wrapper;
+import org.apache.catalina.startup.Tomcat;
+
+class App {
+
+  public static void main(String[] args) throws LifecycleException {
+    Tomcat tomcat = new Tomcat();
+    tomcat.setPort(8080);
+    tomcat.getConnector();
+
+    Context ctx = tomcat.addContext("", null);
+    Wrapper servlet = Tomcat.addServlet(ctx, "myFirstServlet", new MyFirstServlet());
+    servlet.setLoadOnStartup(1);
+    servlet.addMapping("/*");
+
+    tomcat.start();
+  }
+
+  private static class MyFirstServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+        throws IOException {
+      res.setContentType("application/json; charset=UTF-8");
+      res.getWriter().println("{\"message\": \"hello world\"}");
+    }
+  }
+}
