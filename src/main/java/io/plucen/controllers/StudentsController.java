73a1983bc81dcbd7da0ad00445e5b6caf471b4cd
Daniel Plucenio <dplucenio@gmail.com>
2020-08-09 23:14:38 -0300

    remove dummy student creation

--- src/main/java/io/plucen/controllers/StudentsController.java
+++ src/main/java/io/plucen/controllers/StudentsController.java
@@ -1,6 +1,5 @@
 package io.plucen.controllers;
 
-import io.plucen.entities.Student;
 import io.plucen.services.StudentService;
 import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
@@ -18,10 +17,7 @@ public class StudentsController {
   }
 
   public void post(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    Student duque = studentService.create("Duque");
-    System.out.println(duque);
     response.setContentType("application/json; charset=UTF-8");
     response.getWriter().println("{\"message\": \"students post route\"}");
-
   }
 }
