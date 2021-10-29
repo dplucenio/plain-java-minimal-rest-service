3a3e2cbf41ebcbe9af65e10ba1d31a4d480fb4ac
Daniel Plucenio <dplucenio@gmail.com>
2021-10-29 18:54:11 -0300

    wip

--- src/main/java/io/plucen/App.java
+++ src/main/java/io/plucen/App.java
@@ -20,14 +20,13 @@ import org.apache.catalina.startup.Tomcat;
 
 public class App {
 
-  @Getter()
-  private static final ObjectMapper objectMapper = new ObjectMapper();
+  @Getter() private static final ObjectMapper objectMapper = new ObjectMapper();
 
-  private static final Map<Pair<String, HttpMethods>, Controller> controllers = Map
-      .of(Pair.of("/", GET), Configuration.getDashBoardController()::get,
+  private static final Map<Pair<String, HttpMethods>, Controller> controllers =
+      Map.of(
+          Pair.of("/", GET), Configuration.getDashBoardController()::get,
           Pair.of("/students", GET), Configuration.getStudentsController()::get,
-          Pair.of("/students", POST), Configuration.getStudentsController()::post
-      );
+          Pair.of("/students", POST), Configuration.getStudentsController()::post);
 
   public static void main(String[] args) throws LifecycleException {
     Tomcat tomcat = new Tomcat();
@@ -35,26 +34,27 @@ public class App {
     tomcat.getConnector();
     Context context = tomcat.addContext("", null);
 
-    Wrapper servlet = Tomcat.addServlet(context, "minimalServlet", new HttpServlet() {
-      @Override
-      protected void doGet(HttpServletRequest request, HttpServletResponse response)
-          throws IOException {
-        String url = request.getRequestURI().toLowerCase();
-        controllers.getOrDefault(Pair.of(url, GET), (req, res) -> {
-        }).execute(request, response);
-      }
-
-      @Override
-      protected void doPost(HttpServletRequest request, HttpServletResponse response)
-          throws IOException {
-        String url = request.getRequestURI().toLowerCase();
-        controllers.getOrDefault(Pair.of(url, POST), (req, res) -> {
-        }).execute(request, response);
-      }
-    });
+    Wrapper servlet = Tomcat.addServlet(context, "minimalServlet", new MyHttpServlet());
     servlet.setLoadOnStartup(1);
     servlet.addMapping("/*");
 
     tomcat.start();
+  }
+
+  private static class MyHttpServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws IOException {
+      String url = request.getRequestURI().toLowerCase();
+      controllers.getOrDefault(Pair.of(url, GET), (req, res) -> {}).execute(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws IOException {
+      String url = request.getRequestURI().toLowerCase();
+      controllers.getOrDefault(Pair.of(url, POST), (req, res) -> {}).execute(request, response);
+    }
   }
 }
